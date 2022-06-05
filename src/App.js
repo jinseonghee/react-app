9c8379c317f9e202246df99546201cbbd941d220
jinseonghee <wlstjdgml0610@gmail.com>
2022-06-05 22:04:58 +0900

    feat: create components

---
+++ src/App.js
@@ -0,0 +1,32 @@
+//import './App.css';
+import { Component } from 'react';
+import { extend } from 'colord';
+import TOC from './components/TOC';
+import Subject from './components/Subject';
+import Content from './components/Content';
+
+
+class App extends Component {
+  constructor(props) { //component의 render 함수가 실행 되기 전 component를 초기화 시켜주고 싶은 값들은 constructor에 넣어준다.
+    super(props);
+    this.state = {
+      subject : { title : 'WEB', sub : 'World Wide Web' },
+    }
+  }
+
+  render() {
+  return ( //react는 하나의 태그 안쪾에 나머지 태그가 있어야 함. 기징 바깥쪽에는 태그 하나만 있어야 한다.
+    <div className="App"> 
+      <Subject title = {this.state.subject.title} 
+               sub = {this.state.subject.sub} >
+      </Subject>
+      <Subject title = "React" sub = "World Wide react" ></Subject>
+      <TOC></TOC>
+      <Content title = "HTML" desc = "HTML is hyperText Markup Language."></Content>
+    </div>
+    );
+  }
+}
+
+export default App;
+//export default 를 사용함으로써 App 안에 있는 변수나 함수들을 외부에서 사용할 수 있도록 해줌
