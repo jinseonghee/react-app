875a62ca933341bed054a77720ce66f7e51a989b
jinseonghee <wlstjdgml0610@gmail.com>
2022-06-06 19:04:44 +0900

    feat: Implement create(shouldComponentUpdate )

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,6 +2,17 @@ import { Component } from 'react';
 //import './App.css';
 
 class TOC extends Component {
+  shouldComponentUpdate(newProps, newState) { // 이 함수는 새롭게 바뀐 값과 이전값에 접근이 가능
+    console.log('===> TOC render shoudlComponentUpdate'
+    ,newProps.data
+    ,this.props.data
+    );
+    if(this.props.data === newProps.data) { //새롭게 바뀐 값이 없다면 render함수가 다시 호출 되지 않도록 함.
+        return false;
+    }
+    return true; // return 값이 true이면 render 함수가 호출, false 이면 render 가 호출 되지 않음.
+  }
+
     render() {
       var lists = [];
       var data = this.props.data; //App.js의 render 함수 안에 props로 data가 선언되어 있음.
